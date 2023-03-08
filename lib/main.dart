e0516b6196b3e6bbb49cd98543ce593db199b4bd
Azis Zuhri Pratomo <aziszuhrip354@gmail.com>
2023-03-08 22:11:43 +0700

    update praktikum 2 - percobaan 1

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  String _text = "";
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -61,13 +60,6 @@ class _MyHomePageState extends State<MyHomePage> {
         _counter = 0;
       }
       _counter++;
-
-      if(_counter % 2  == 0) {
-        _text = "Genap";
-      } else {
-        _text = "Ganjil";
-      }
-
     });
   }
 
@@ -106,14 +98,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Cek bilangan ganjil atau genap',
+              'Tekan button (+) 1 sampai 10',
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              _text,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
